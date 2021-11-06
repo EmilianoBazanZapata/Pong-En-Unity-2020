5b5e2e791b15e500952abbe3e4371fe417c64574
EmilianoBazanZapata <57721403+EmilianoBazanZapata@users.noreply.github.com>
2021-11-05 22:36:54 -0300

    Cambiar la dirección de la pelota con la pala

--- Pong/Assets/Scripts/Ball.cs
+++ Pong/Assets/Scripts/Ball.cs
@@ -9,4 +9,44 @@ public class Ball : MonoBehaviour
     {
         GetComponent<Rigidbody2D>().velocity = Vector2.right * Speed;
     }
+    //obtendre informacion del choque de la bola
+    //para saber si he chocado con el objeto correcto o no
+    //en este caso debo verificar si choca con la pala
+    /*
+      1 di choca con la parte superior
+      0 si choca con el medio
+      -1 si choca con la parte inferior
+    */
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "PlayerLeft")
+        {
+            float y = HitFactor(//obtengo la posicion de la bola
+                                this.transform.position,
+                                //obtengo la posicion de la pala
+                                collision.transform.position,
+                                //obtengo la altura de la pala
+                                collision.collider.bounds.size.y);
+
+            Vector2 Direction = new Vector2(1, y).normalized;
+            GetComponent<Rigidbody2D>().velocity = Direction * Speed;
+        }
+        if (collision.gameObject.name == "PlayerRight")
+        {
+            float y = HitFactor(//obtengo la posicion de la bola
+                                this.transform.position,
+                                //obtengo la posicion de la pala
+                                collision.transform.position,
+                                //obtengo la altura de la pala
+                                collision.collider.bounds.size.y);
+
+            Vector2 Direction = new Vector2(-1, y).normalized;
+            GetComponent<Rigidbody2D>().velocity = Direction * Speed;
+        }
+    }
+    //metodo que devuelve el factor citado al principio del script
+    private float HitFactor(Vector2 BallPosition, Vector2 RacketPosition, float RaquetHeight)
+    {
+        return (BallPosition.y - RacketPosition.y) / RaquetHeight;
+    }
 }
