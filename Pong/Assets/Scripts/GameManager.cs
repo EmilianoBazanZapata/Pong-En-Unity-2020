91db044e0a67b73507cffcc25281256bce4311e2
EmilianoBazanZapata <57721403+EmilianoBazanZapata@users.noreply.github.com>
2021-11-05 23:47:19 -0300

    El botón para iniciar el juego

--- Pong/Assets/Scripts/GameManager.cs
+++ Pong/Assets/Scripts/GameManager.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager SharedInstance = null;
     public bool GameStarted = false;
+    public Text Title;
+    public Button StartButton;
     private void Awake()
     {
         if (SharedInstance == null)
@@ -13,5 +16,10 @@ public class GameManager : MonoBehaviour
             SharedInstance = this;
         }
     }
-    
+    public void StartGame()
+    {
+        this.GameStarted = true;
+        this.Title.enabled = false;
+        this.StartButton.gameObject.SetActive(false);
+    }
 }
